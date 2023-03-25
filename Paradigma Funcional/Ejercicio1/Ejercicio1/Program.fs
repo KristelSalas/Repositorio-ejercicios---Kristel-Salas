06558ebcf249e75cedf55f97fc95d04706632102
KristelSalas <61443460+KristelSalas@users.noreply.github.com>
2023-03-24 21:33:01 -0600

    Ejercicio1

---
+++ Paradigma Funcional/Ejercicio1/Ejercicio1/Program.fs
@@ -0,0 +1,16 @@
+﻿
+//Kristel Salas Villegas
+
+(* Haciendo uso de la función filter, implemente una función que a partir de una lista de cadenas representando 
+frases de parámetro, filtre aquellas que contengan una palabra que el usuario indique en otro argumento 
+(palabra completa contenida). 
+Ej sub_cadenas “la” [“la casa; “el perro”; “pintando la cerca”]
+    [“la casa, “pintando la cerca”]
+*)
+
+let sub_cadenas (palabra: string) (frases: string list) =
+    frases |> List.filter (fun frase -> frase.Contains(palabra))
+
+let listaFrases = ["el proyecto"; "la tarea"; "el quiz"; "la exposición"]
+
+printfn "%A" (sub_cadenas "el" listaFrases)
